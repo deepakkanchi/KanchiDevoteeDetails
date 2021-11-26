32a34298a38c3513ac4488c37bb88567195de5b9
Deepak Sriram <deesriram@paypal.com>
2021-11-27 02:15:36 +0530

    Pillaiyar Suzhi Added

--- components/StaticContent.jsx
+++ components/StaticContent.jsx
@@ -5,7 +5,10 @@ import Image from 'next/image'
 export default function StaticContent() {
     return (
         <>
-            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }} style={{ marginTop: "20px", color: "#f09724", textShadow: "0.5px 2px #eaed42" }}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{ marginTop: "5px", color: "#f09724", textShadow: "2px 0.5px #eaed42" }}>
+                உ
+            </Typography>
+            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }} style={{ marginTop: "0px", color: "#f09724", textShadow: "0.5px 2px #eaed42" }}>
                 सदा शिव समारमभां शङ्कराचार्य मध्यमाम्॥ अस्मदाचार्य पर्यन्तां वन्दे गुरु परम्पराम्॥
             </Typography>
             <div style={{ marginTop: "20px" }}>
